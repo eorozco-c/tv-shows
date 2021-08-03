3663481fce8b5b4b0d6c5b51d3d03113a8a5aad8
Ernesto Orozco C <ernestorozco94@gmail.com>
2021-08-02 20:07:26 -0400

    sec commit

--- tv_shows/settings.py
+++ tv_shows/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mu^+i^l=f9eabt6%3fbm%8x=)90d-_lj0p0!(@w54+=#&t9mb^'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
