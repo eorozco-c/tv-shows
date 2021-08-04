f989f2cffb1c548cf859d3173d4f5323ad18fc95
Ernesto Orozco C <ernestorozco94@gmail.com>
2021-08-04 02:06:29 -0400

    Add Validations with django model.Manager

--- apps/shows/models.py
+++ apps/shows/models.py
@@ -1,6 +1,26 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        release_date =  datetime.datetime.strptime(postData['release_date'], '%Y-%m-%d').date()
+        today = datetime.date.today()
+        if len(postData['title']) < 2:
+            errors["title"] = "El titulo debe tener minimo 2 caracteres"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network debe tener minimo 3 caracteres"
+        if len(postData['description']) < 10 and postData['description'] != "":
+            errors["description"] = "Description debe tener minimo 10 caracteres"
+        if  release_date >= today:
+            print(release_date, today)
+            errors["release_date"] = "Fecha debe ser menor al dia de hoy"
+        for title in Show.objects.all():
+            if postData['title'] == title.title:
+                errors["release_date"] = "Ya existe ese titulo"
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=50)
     network = models.CharField(max_length=20)
@@ -8,3 +28,5 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
--- apps/shows/views.py
+++ apps/shows/views.py
@@ -1,7 +1,8 @@
+from os import error
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render,HttpResponse
 from .models import Show
-import datetime
+from django.contrib import messages
 
 # Create your views here.
 def root(request):
@@ -20,13 +21,20 @@ def new(request):
 
 def create(request):
     if request.method == "POST":
-        title = request.POST["title"]
-        network = request.POST["network"]
-        releaseDate = request.POST["releaseDate"]
-        description = request.POST["description"]
-        Show.objects.create(title=title,network=network,release_date=releaseDate,description=description)
-    lastshow = Show.objects.last()
-    return redirect(f"/shows/{lastshow.id}")
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirigir al usuario al formulario para corregir los errores
+            return redirect(f'/shows/new')
+        else:
+            title = request.POST["title"]
+            network = request.POST["network"]
+            release_date = request.POST["release_date"]
+            description = request.POST["description"]
+            Show.objects.create(title=title,network=network,release_date=release_date,description=description)
+        lastshow = Show.objects.last()
+        return redirect(f"/shows/{lastshow.id}")
 
 def shows(request,id):
     if request.method == "GET":
@@ -44,13 +52,20 @@ def edit(request,id):
 
 def update(request,id):
     if request.method == "POST":
-        show = Show.objects.get(id=id)
-        show.title = request.POST["title"]
-        show.network = request.POST["network"]
-        show.releaseDate = request.POST["releaseDate"]
-        show.description = request.POST["description"]
-        show.save()
-    return redirect(f"/shows/{show.id}")
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirigir al usuario al formulario para corregir los errores
+            return redirect(f'/shows/{id}/edit')
+        else:
+            show = Show.objects.get(id=id)
+            show.title = request.POST["title"]
+            show.network = request.POST["network"]
+            show.release_date = request.POST["release_date"]
+            show.description = request.POST["description"]
+            show.save()
+            return redirect(f"/shows/{show.id}")
 
 def destroy(request,id):
     show = Show.objects.get(id=id)
